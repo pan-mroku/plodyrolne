75f9a7919cac957e108fb3b5bbe0541cd3ca0588
Paweł Szymański <out.of.logins.error@gmail.com>
2015-05-21 00:04:52 +0200

    mapa w profilu

---
+++ static/gmaps_profil.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+		(function(){
+				GMaps.geocode({
+						address: $('#Adres').text().trim(),
+						callback: function(results, status){
+								if(status=='OK'){
+										var latlng= results[0].geometry.location;
+										map = new GMaps({
+												lat: latlng.lat(),
+												lng: latlng.lng(),
+												div: '#gmap',
+												zoom: 6
+										});
+										map.addMarker({
+												lat: latlng.lat(),
+												lng: latlng.lng(),
+										});
+								}
+						}
+				});
+		})();
+});
