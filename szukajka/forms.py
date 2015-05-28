f798a604ea2d57d058f4bf1af592fc0eb184ff55
Paweł Szymański <out.of.logins.error@gmail.com>
2015-05-28 21:03:22 +0200

    szukaj: positiveintegerfield

--- szukajka/forms.py
+++ szukajka/forms.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 #co za absurd. nie wiem jak zrobić mapę bez używania modelformów. Czysty forms.charfield nie ma atrybutu name
 class Szukaj(models.Model):
-    Odległość = models.IntegerField(default=10)
+    Odległość = models.PositiveIntegerField(default=10)
     Adres = models.CharField(max_length = 50)
 
 
